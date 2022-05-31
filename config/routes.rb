50d4d166e57d497cab701a5c171d08237f120c0d
Florian Schneider schneider.f@me.com <schneider.f@me.com>
2022-05-31 13:09:29 +0200

    form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :carehomes do
-    resources :reviews
+    resources :reviews, only: [:create]
     resources :favorites, only: [:create]
     resources :chatrooms, only: [:create]
   end
