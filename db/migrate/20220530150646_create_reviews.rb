9464331d8a8c32df87c704be448c7a62d1aad29f
Florian Schneider schneider.f@me.com <schneider.f@me.com>
2022-05-31 14:34:58 +0200

    fixed model datatypes

--- db/migrate/20220530150646_create_reviews.rb
+++ db/migrate/20220530150646_create_reviews.rb
@@ -2,12 +2,12 @@ class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
       t.text :content
-      t.string :rating
-      t.string :food
-      t.string :privacy
-      t.string :staff
-      t.string :hygiene
-      t.string :atmosphere
+      t.integer :rating
+      t.integer :food
+      t.integer :privacy
+      t.integer :staff
+      t.integer :hygiene
+      t.integer :atmosphere
       t.references :user, null: false, foreign_key: true
       t.references :carehome, null: false, foreign_key: true
 
