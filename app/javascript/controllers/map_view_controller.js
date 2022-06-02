468370f22cffd741d85fae8a325d8e7634df1394
Julien Rolland <rollandjulien13013@gmail.com>
2022-06-02 15:15:29 +0200

    add photos to the layout

--- app/javascript/controllers/map_view_controller.js
+++ app/javascript/controllers/map_view_controller.js
@@ -8,7 +8,6 @@ export default class extends Controller {
   }
 
   connect() {
-    console.log(this.markerValue, this.markerValue.lat)
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
