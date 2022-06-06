fcb76b22748a720c743437421e01f3c12b6235d9
Dreka Njaramba <drekanj@outlook.de>
2022-06-06 11:25:27 +0200

    fix reviewspage

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :carehome
 
-  validates :content, :rating, presence: true
-  validates :rating, numericality: { in: 1..5 }
+  validates :content, :rating, :privacy, :hygiene, :atmosphere, :staff, :food, presence: true
 end
