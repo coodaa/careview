cfd8a5f41670dcee5e191abefb1b11c41a8a893b
Julien Rolland <rollandjulien13013@gmail.com>
2022-06-07 15:04:31 +0200

    fix if there is no reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -28,21 +28,23 @@ class ReviewsController < ApplicationController
     }
 
     # Calculate average rating for the carehome
-    total_reviews = @carehome.reviews.count
-    count = 0
-    @carehome.reviews.each do |review|
-      count += review.average_ratings
-    end
-    average_rating = count / total_reviews
+    unless @carehome.reviews.empty?
+      total_reviews = @carehome.reviews.count
+      count = 0
+      @carehome.reviews.each do |review|
+        count += review.average_ratings
+      end
+      average_rating = count / total_reviews
 
-    @ratings = {
-      rating: @carehome.average_rating.round(1),
-      food: @carehome.reviews.average(:food).to_f.round(1),
-      privacy: @carehome.reviews.average(:privacy).to_f.round(1),
-      staff: @carehome.reviews.average(:staff).to_f.round(1),
-      hygiene: @carehome.reviews.average(:hygiene).to_f.round(1),
-      atmosphere: @carehome.reviews.average(:atmosphere).to_f.round(1)
-    }
+      @ratings = {
+        rating: average_rating.round(1),
+        food: @carehome.reviews.average(:food).to_f.round(1),
+        privacy: @carehome.reviews.average(:privacy).to_f.round(1),
+        staff: @carehome.reviews.average(:staff).to_f.round(1),
+        hygiene: @carehome.reviews.average(:hygiene).to_f.round(1),
+        atmosphere: @carehome.reviews.average(:atmosphere).to_f.round(1)
+      }
+    end
 
     @reviews = @carehome.reviews
     @reviews_last = @reviews.drop(2)
