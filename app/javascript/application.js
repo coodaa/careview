b0633b291916dec5a1f912d75362877650d08cfa
Dreka Njaramba <drekanj@outlook.de>
2022-06-09 13:57:23 +0200

    sweeetalert

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,13 @@ import "bootstrap"
 import { initStarRating } from './controllers/init_star_rating';
 
 initStarRating();
+
+import { initSweetalert } from './controllers/init_sweetalert';
+
+initSweetalert('#sweet-alert', {
+  title: "Thanks",
+  text: "The Message was send",
+  icon: "success"
+}, (value) => {
+  console.log(value);
+});
